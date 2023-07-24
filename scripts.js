b129b93456193bbb551faf74775a64f73484449e
brunomaiasl <brunomaiadasilva@gmail.com>
2023-07-24 20:19:00 -0300

    Primeiro commit - conversor

---
+++ scripts.js
@@ -0,0 +1,104 @@
+
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+const currencyFirstSelect = document.querySelector(".currency-first-select")
+
+
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em real
+    const currencyValueConverted = document.querySelector(".currency-value") //Outras moedas
+
+
+    const dolarToday = 4.8
+    const euroToday = 6.2
+    const libraToday = 6.1
+    const bitcoinToday = 126464.6
+
+
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+
+    if (currencySelect.value == "bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrencyValue / bitcoinToday)
+    }
+
+
+
+}
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+  
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar"
+        currencyImage.src = "./assets/dolar.png"
+    }
+
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+    }
+
+    if (currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bicoin"
+        currencyImage.src = "./assets/bitcoin.png"
+    }
+
+    convertValues()
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
+
+
+/*const request = require('request')
+const moedas = inputCurrencyValue
+
+
+const options = {
+    url:`https://economia.awesomeapi.com.br/last/${moedas}`,
+    method:'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8'
+    }
+}
+
+const callback_dolar = function(erro, res, body){
+    let json = JSON.parse(body)
+    cotacao = json.USBRL['bid']
+}*/
+
